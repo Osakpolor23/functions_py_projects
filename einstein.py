f4840b7999ff55880cc86617ceec8e0b18a777f9
Osakpolor Ogieriakhi <osakpolorogieriakhi@gmail.com>
2025-04-03 15:56:36 +0100

    Added this file

---
+++ einstein.py
@@ -0,0 +1,17 @@
+def main():
+    # Take user's input for mass in kilogram
+    m = int(input("m: "))
+    # Assign the default speed of light
+    c = 300000000
+    # Calculate the value of Energy
+    E = m * pow(c,2)
+    # Print the output
+    print(f"E: {E}")
+
+# Call the main function
+main()
+
+
+
+
+
