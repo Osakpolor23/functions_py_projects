fe955ebade61c4692a78898f22674b47e03a5a82
Osakpolor Ogieriakhi <osakpolorogieriakhi@gmail.com>
2025-04-04 05:34:31 +0100

    Added this file

---
+++ indoor.py
@@ -0,0 +1,10 @@
+# Define the main function
+def main():
+    user_input = input("Enter your words here: ")
+    # change all characters to lowercase
+    user_output = user_input.lower()
+    # Print the result
+    print(user_output)
+
+# Call the main function
+main()
